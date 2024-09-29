0d2cd0c927c5640966ad208bb8f179e074304916
LuisFelipeA <felipeadriani2@gmail.com>
2024-09-29 12:45:43 -0300

    Conexão com banco de dados e criação tabela User

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 
+import { PrismaClient } from "@prisma/client";
+
 export const userRouter = Router();
 
 const router = Router();
 
 userRouter.use('/user', router);
 
-router.get('/', (req, res) => {
-    res.send('Hello World user!')
+router.get('/', async function (req, res){
+    const prisma = new PrismaClient();
+    const users = await prisma.user.findMany();
+    res.send(users);
 })
 
 router.get('/:nome', (req, res) => {
